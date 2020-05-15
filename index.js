e5274fff549d6d8b2cf7a11b31ac0eb41058b0cd
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-05-15 23:24:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+const main = document.querySelector("#main");
+main.remove();
+
+const h1 = document.createElement("h1");
+h1.id="victory";
+h1.innerHTML="H1";
+document.body.appendChild(h1);
+h1.innerHTML="Abdulbasit is the champion";
+const newHeader = document.querySelector("h1#victory");
