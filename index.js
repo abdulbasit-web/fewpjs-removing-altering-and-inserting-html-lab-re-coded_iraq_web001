e81f502956bf5be4ac0d3bde66a5189a50a85f90
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-05-15 23:19:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // Write your code here
-const main = document.querySelector("#main");
-main.remove();
-
-const h1 = document.createElement("h1");
-h1.innerHTML="<h1 id='victory'>H1</h1>";
-body.appendChild(h1);
-const newHeader = document.querySelector("h1#victory");
