c0e142e03f839c9ca9ae5232e5ceae56d7efc55c
Abdulbasit Salah <abdullbasit99salah@gmail.com>
2020-05-15 23:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code here
+const main = document.querySelector("#main");
+main.remove();
+
+const h1 = document.createElement("h1");
+h1.id="victory";
+h1.textContent = "dgd"
+main.appendChild(h1);
+const newHeader = document.querySelector("h1#victory");
